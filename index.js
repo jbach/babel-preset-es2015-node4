3994bf5e05cecad55a72ea7a936de26f126e4179
Jonas Bach <hallo@jonasbach.de>
2015-11-04 16:55:06 +0100

    add plugins

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: [
+		require('babel-plugin-transform-es2015-sticky-regex'),
+		require('babel-plugin-transform-es2015-unicode-regex'),
+		require('babel-plugin-transform-es2015-spread'),
+		require('babel-plugin-transform-es2015-parameters'),
+		require('babel-plugin-transform-es2015-destructuring'),
+		require('babel-plugin-transform-es2015-typeof-symbol'),
+		require('babel-plugin-transform-es2015-modules-commonjs')
+	]
+}
